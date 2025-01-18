44c5ecc7226b39ba1a0a19c521d9f61c85d49888
Md Ashfaque <103313835+mdashfaquemarch@users.noreply.github.com>
2025-01-18 21:23:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-<<<<<<< HEAD
-=======
-  server: {
-    proxy: {
-      "/api/v1": "https://paytm-backend-m8l4.onrender.com"
-    }
-  },
->>>>>>> 530f0353c3063683615bfe042c42b257f343b8f5
+
   plugins: [react()],
   resolve: {
     alias: {
